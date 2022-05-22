f080ee615c18860644613d08295cbb26a960e4a8
Andreas Falk <info@andreas-falk.de>
2022-05-22 17:55:32 +0200

    fixed setup section

--- setup/run_keycloak.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-setlocal
-
-rem replace this with your own installation directory of keycloak
-set KEYCLOAK_HOME=C:\keycloak-12.0.4
-
-%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.profile=preview
